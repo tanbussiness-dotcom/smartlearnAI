6d0d2bc666af1c1d767b6e101652433618d30010
Mai Minh Tân <maiminhtan2303@gmail.com>
2025-11-09 05:22:21 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2850149740-400d0",
+  "appId": "1:914247387415:web:000d79a9a172b8e2624f52",
+  "apiKey": "AIzaSyDK1qHKyXNZqlG-wWUAESJ-zYJh2p_uhs4",
+  "authDomain": "studio-2850149740-400d0.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "914247387415"
+};
