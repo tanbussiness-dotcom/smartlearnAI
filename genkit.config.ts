32d7ecae54316bab61ca2d9d0fb38cb3fae6ae95
Mai Minh Tân <maiminhtan2303@gmail.com>
2025-11-10 05:18:29 +0000

    Try fixing this error: `Console Error: Error: Failed to fetch from https

--- genkit.config.ts
+++ genkit.config.ts
@@ -1,13 +1,10 @@
+
 import { genkit } from 'genkit';
 import { googleAI } from '@genkit-ai/google-genai';
 
 export const ai = genkit({
   plugins: [
-    googleAI({
-      projectId: 'smartlearn-ai',
-      location: 'us-central1',
-      keyFile: './vertex-ai-admin.json',
-    }),
+    googleAI(),
   ],
   logLevel: 'debug',
   enableTracingAndMetrics: true,
