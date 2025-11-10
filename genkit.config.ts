858667cce3eba519fa5f95e44fadc3976e600c19
Mai Minh Tân <maiminhtan2303@gmail.com>
2025-11-10 05:21:38 +0000

    Error: Failed to fetch from https://generativelanguage.googleapis.com/v1

--- genkit.config.ts
+++ genkit.config.ts
@@ -4,7 +4,10 @@ import { googleAI } from '@genkit-ai/google-genai';
 
 export const ai = genkit({
   plugins: [
-    googleAI({ keyFile: 'vertex-ai-admin.json' }),
+    googleAI({ 
+      keyFile: 'vertex-ai-admin.json',
+      apiVersion: 'v1',
+     }),
   ],
   logLevel: 'debug',
   enableTracingAndMetrics: true,
